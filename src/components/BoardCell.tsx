e68fde41d6a2f91df425a88facba34969afba702
haoyudong <2070343054@email.szu.edu.cn>
2023-03-31 02:05:03 +0800

    chore: refract css

--- src/components/BoardCell.tsx
+++ src/components/BoardCell.tsx
@@ -6,15 +6,42 @@ interface BoardCell {
 }
 
 export default function BoardCell({ row, col }: BoardCell) {
-    const [showTopLeft, setShowTopLeft] = useState(false)
-    const handleTopLeftClick = () => {
-        setShowTopLeft((prevState) => {
-            return !prevState
-        })
-    };
-    return (
-        <div key={`${row}-${col}`} className="board-cell" onClick={() => handleTopLeftClick()}>
-            <div className='top-left' style={{ display: showTopLeft ? "block" : "none" }} ></div>
-        </div>
-    )
+    if (row === 0 && col === 0) {
+        return (
+            <div key={`${row}-${col}`} className="board-cell top-left"></div >
+        )
+    } else if (row === 0 && col === 14) {
+        return (
+            <div key={`${row}-${col}`} className="board-cell top-right"></div >
+        )
+    } else if (row === 14 && col === 0) {
+        return (
+            <div key={`${row}-${col}`} className="board-cell bottom-left"></div >
+        )
+    } else if (row === 14 && col === 14) {
+        return (
+            <div key={`${row}-${col}`} className="board-cell bottom-right"></div >
+        )
+    } else if (row === 0 && col !== 0 && col !== 14) {
+        return (
+            <div key={`${row}-${col}`} className="board-cell top-middle"></div >
+        )
+    } else if (row === 14 && col !== 0 && col !== 14) {
+        return (
+            <div key={`${row}-${col}`} className="board-cell bottom-middle"></div >
+        )
+    } else if (col === 0 && row !== 14 && row !== 0) {
+        return (
+            <div key={`${row}-${col}`} className="board-cell left-middle"></div >
+        )
+    } else if (col === 14 && row !== 14 && row !== 0) {
+        return (
+            <div key={`${row}-${col}`} className="board-cell right-middle"></div >
+        )
+    }
+    else {
+        return (
+            <div key={`${row}-${col}`} className="board-cell middle"></div >
+        )
+    }
 }
