68196948cab798b8401b25c2d9911e1bd72a7834
haoyudong <2070343054@email.szu.edu.cn>
2023-03-31 03:32:01 +0800

    chore: update config

--- src/components/BoardLine.tsx
+++ src/components/BoardLine.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { POSITIONS } from '../constants/config'
+import { BOARD_SIZE, POSITIONS } from '../constants/config'
 import { IRowCol } from '../types';
 import '../styles/BoardLine.css'
 export default function BoardLine({ row, col }: IRowCol) {
-    const position = POSITIONS[`${row}-${col}` as '0-0' | '0-14' | '14-0' | '14-14']
+    const position = POSITIONS[`${row}-${col}` as any]
     const boardClassName = position || (
         row === 0 ? 'top-middle' :
-            row === 14 ? 'bottom-middle' :
+            row === BOARD_SIZE - 1 ? 'bottom-middle' :
                 col === 0 ? 'left-middle' :
-                    col === 14 ? 'right-middle' :
+                    col === BOARD_SIZE - 1 ? 'right-middle' :
                         ''
     );
     return (
--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,7 +1,8 @@
 export const BOARD_SIZE = 15; // 棋盘大小
 export const POSITIONS = {
     '0-0': 'top-left',
-    '0-14': 'top-right',
-    '14-0': 'bottom-left',
-    '14-14': 'bottom-right'
+    [`0-${BOARD_SIZE - 1}`]: 'top-right',
+    [`${BOARD_SIZE - 1}-0`]: 'bottom-left',
+    [`${BOARD_SIZE - 1}-${BOARD_SIZE - 1}`]: 'bottom-right'
 } as const;
+
