009d86b8ea381a942c5b58559add7284e1e2466a
haoyudong <2070343054@email.szu.edu.cn>
2023-04-02 02:50:05 +0800

    fix: remove Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Board from './components/Board'
-import Header from './components/Header'
 import './styles/App.css'
 function App() {
 
   return (
     <>
-      {/* <Header /> */}
       <div className="App">
         <Board></Board>
       </div>
